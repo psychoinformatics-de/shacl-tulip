52237bc8a8fee1fe5f80239cc3840584de50df61
Stephan Heunis <s.heunis@fz-juelich.de>
2025-04-29 16:53:38 +0200

    Updates error handling and request responses
    This changes the adhoc way in which request responses were handled
    into a structured handling approach, where errors are thrown and
    caught and returned as an object with standard properties, such as
    'success', 'message', 'error', 'skipped', 'url'. This holds for
    successful requests as well. This allows all errors to be bubbled
    upwards in a structured and consistent way, so that frontend apps
    that use this library can react to request responses in a systematic
    way.

--- src/modules/io.js
+++ src/modules/io.js
@@ -13,9 +13,11 @@ import formatsPretty from '@rdfjs/formats/pretty.js'
 import rdf from 'rdf-ext'
 
 export async function readRDF(file_url, headers = { "Content-Type": "text/turtle" }) {
+    const url = file_url;
     try {
+        console.log(url)
         var res = null
-        res = await fetch(file_url,
+        res = await fetch(url,
             {
                 formats, 
                 headers: headers
@@ -25,48 +27,36 @@ export async function readRDF(file_url, headers = { "Content-Type": "text/turtle
         if (['text/plain', 'text/html'].indexOf(res.headers.get('content-type')) >= 0) {
             // default to turtle
             headers['Content-Type'] = 'text/turtle';
-            res = await fetch(file_url, { formats, headers });
+            res = await fetch(url, { formats, headers });
         }
-        if (res.ok) {
-            if (res.headers.get('content-type').indexOf('application/json') >= 0) {
-                console.log(res.json())
-                throw new Error(`readRDF error: cannot read json data`);
-            }
-            const quadStream = await res.quadStream()
-            // quadStream.on('error', err => console.error(err))
-            return quadStream
-        } else {
-            throw new Error(`readRDF error:: ${res.statusText}`);
-        }
-    } catch (error) {
-        console.error('readRDF error:', error);
-        throw error;
-    }
-}
 
-
-export async function postRDF(endpoint, dataset, format = 'text/turtle', headers = {}, prefixes) {
-    
-    try {
-        const rdfPretty = rdf.clone() // clone the default environment
-        rdfPretty.formats.import(formatsPretty) // import pretty print serializers
-        // Ensure we have the correct content-type
-        headers['Content-Type'] = format;
-        // Serialize the dataset to the desired format
-        const body = await rdfPretty.io.dataset.toText('text/turtle', dataset)
-        const response = await fetch(endpoint, {
-            method: 'POST',
-            formats, 
-            headers,
-            body,
-            prefixes,
-        });
-        if (!response.ok) {
-            throw new Error(`postRDF error: ${response.statusText}`);
+        if (!res.ok) {
+            // throw new Error(`readRDF error: ${res.statusText}`)
+            const code = res.status || 'Unknown';
+            const error = new Error(`readRDF error: HTTP ${code} from ${url}`);
+            error.status = code;
+            error.url = url;
+            error.response = res;
+            throw error
         }
-        return response;
+
+        if (res.headers.get('content-type').indexOf('application/json') >= 0) {
+            throw new Error(`readRDF error: reading json data is not supported`);
+        }
+
+        const quadStream = await res.quadStream()
+        return {
+            success: true,
+            quadStream,
+            url: url,
+            message: 'RDF data loaded successfully',
+        };
     } catch (error) {
-        console.error('postRDF error:', error);
-        throw error;
+        return {
+            success: false,
+            error,
+            url: url,
+            message: error.message,
+        };
     }
 }
